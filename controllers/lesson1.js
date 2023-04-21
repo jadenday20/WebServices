ef4ad36646a36d2bbba1f64ad35757882655e736
jadenday20 <day18007@byui.edu>
2023-04-21 17:38:55 -0600

    added kyler and dallin

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,12 +1,19 @@
 const jadenRoute = (req,res) => {
     res.send("Jaden Day");
-  };
-
-  const elishaRoute = (req,res) => {
-    res.send("Elisha Day");
-  }
+};
+const elishaRoute = (req,res) => {
+res.send("Elisha Day");
+}
+const dallinRoute = (req,res) => {
+res.send("Dallin Day");
+}
+const kylerRoute = (req,res) => {
+res.send("Kyler Day");
+}
 
 module.exports = {
     jadenRoute,
-    elishaRoute
+    elishaRoute,
+    dallinRoute,
+    kylerRoute
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1')
 
-routes.get('/', lesson1Controller.jadenRoute);
-routes.get('/elisha', lesson1Controller.elishaRoute);
+routes.get('/', lesson1Controller.elishaRoute);
+routes.get('/dallin', lesson1Controller.dallinRoute);
+routes.get('/kyler', lesson1Controller.kylerRoute);
 
 module.exports = routes;
