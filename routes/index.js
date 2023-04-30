c3096ab14e02345c5eab4619bbd52ca967326578
jadenday20 <day18007@byui.edu>
2023-04-29 23:50:59 -0600

    added some changes to my code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1')
 
-routes.get('/', lesson1Controller.elishaRoute);
-routes.get('/dallin', lesson1Controller.dallinRoute);
-routes.get('/kyler', lesson1Controller.kylerRoute);
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
