1496bfa255625e25e8b2173dc3165a6219dc1767
jadenday20 <day18007@byui.edu>
2023-05-06 16:40:01 -0600

    added the delete update and add functionalities

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,15 +1,15 @@
-const elishaRoute = (req,res) => {
-res.send("Elisha Day");
-}
-const dallinRoute = (req,res) => {
-res.send("Dallin Day");
-}
-const kylerRoute = (req,res) => {
-res.send("Kyler Day");
-}
+const elishaRoute = (req, res) => {
+  res.send("Elisha Day");
+};
+const dallinRoute = (req, res) => {
+  res.send("Dallin Day");
+};
+const kylerRoute = (req, res) => {
+  res.send("Kyler Day");
+};
 
 module.exports = {
-    elishaRoute,
-    dallinRoute,
-    kylerRoute
+  elishaRoute,
+  dallinRoute,
+  kylerRoute,
 };
