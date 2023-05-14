4dbabee7c33d2ab29abf8a82f7cfb75f03e262d0
jadenday20 <day18007@byui.edu>
2023-05-13 22:39:24 -0600

    added swagger documentation

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
 const port = process.env.PORT || 8080;
 const app = express();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
-  .use(bodyParser.json())//makes it so I can use less code by using req, next, and things like that
+  .use(bodyParser.json()) //makes it so I can use less code by using req, next, and things like that
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use('/', require('./routes'));
+  .use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
