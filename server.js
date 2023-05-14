87a1dc8a01b408c7125104d7f67f9891819c8290
jadenday20 <day18007@byui.edu>
2023-05-13 23:02:58 -0600

    added code to enable http requests

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app
   .use(bodyParser.json()) //makes it so I can use less code by using req, next, and things like that
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     next();
   })
   .use("/", require("./routes"));
